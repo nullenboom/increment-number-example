a565cc7d5e63cda270617628a6fef89d380f1511
Niklas Ullenboom <niklas.ullenboom@paluno.uni-due.de>
2020-01-05 19:05:53 +0100

    splitted tests

--- test/IncrementExampleMultiple.js
+++ test/IncrementExampleMultiple.js
@@ -1,19 +1,31 @@
 const IncrementShort = artifacts.require("./IncrementShort.sol");
 const IncrementLong = artifacts.require("./IncrementLong.sol");
 
+const addTimes = getRandomRange(25,50);
+
 contract("IncrementShort", async accounts => {
-  it("Increment 2 Time", async () => {
+  it("Increment Multiple Times", async () => {
     const incrementShort = await IncrementShort.new();
-		await incrementShort.increment();
-		await incrementShort.increment();
 	
+	for(var i =0; i < addTimes; i++){
+	   await incrementShort.increment();
+	}
+    
   });
 });
 
+
 contract("IncrementLong", async accounts => {
-  it("Increment 2 Time", async () => {
+  it("Increment Multiple Times", async () => {
     const incrementLong = await IncrementLong.new();
+	
+	for(var i =0; i < addTimes; i++){
 		await incrementLong.increment();
-		await incrementLong.increment();
+	}	
+    
   });
 });
+
+function getRandomRange(min, max) {
+    return Math.random() * (max - min) + min;
+}
