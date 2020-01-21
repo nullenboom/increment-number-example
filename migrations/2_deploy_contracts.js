391f4272fd189d4b094771a9ffb50fb5aab94c8d
Niklas Ullenboom <niklas.ullenboom@paluno.uni-due.de>
2020-01-21 20:39:06 +0100

    Delets old project

--- migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-var IncrementShort = artifacts.require("./IncrementShort.sol");
-var IncrementLong = artifacts.require("./IncrementLong.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(IncrementShort);
-  deployer.deploy(IncrementLong);
-};
